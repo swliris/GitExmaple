f16a5a58d7e735cfb3a73ba2749fb10f8a8a80f3
Clouds <swliris24@gmail.com>
2019-07-19 11:23:35 +0900

    hi

--- app/src/main/java/com/limsungwoo/gitexmaple/MainActivity.java
+++ app/src/main/java/com/limsungwoo/gitexmaple/MainActivity.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int i = 0;
     }
+
 }
